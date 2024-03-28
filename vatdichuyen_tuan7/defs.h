416b4029b4b4ce0fd276c7b1f634c66e8dc6ceda
LeHoangLinh05 <hzyugioh@gmail.com>
2024-03-28 18:38:28 +0700

    tuan7

---
+++ vatdichuyen_tuan7/defs.h
@@ -0,0 +1,11 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const char* WINDOW_TITLE = "Hello World!";
+#define BOARD_X 10
+#define BOARD_Y 10
+#define CELL_SIZE 30
+
+#endif
