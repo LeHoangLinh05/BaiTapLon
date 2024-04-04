edf7d5510e8ce58ef87fde7f66fcf2f982e9a03b
LeHoangLinh05 <hzyugioh@gmail.com>
2024-04-04 19:14:57 +0700

    tuan8

---
+++ hoathinh_background_tuan8/defs.h
@@ -0,0 +1,16 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define WINDOW_TITLE "Hello World!"
+
+#define BACKGROUND_IMG "img\\forest.jpg"
+#define BACKGROUND_IMG "img\\123456.png"
+
+
+
+
+
+#endif // DEFS_H_INCLUDED
