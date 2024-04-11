128197fd2aa76125347e6c89f65ba6124c36b58d
LeHoangLinh05 <hzyugioh@gmail.com>
2024-04-11 21:58:28 +0700

    text

---
+++ text/defs.h
@@ -0,0 +1,11 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const char* WINDOW_TITLE = "Hello World!";
+
+
+
+#endif // DEFS_H_INCLUDED
